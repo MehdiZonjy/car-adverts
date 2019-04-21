f0769bd7549e9f289a2a9a6d2e5f08b2f6277bb1
Mehdi Zonjy <mehdizonjy@hotmail.com>
2019-04-21 18:51:38 +0800

    fixes docker dynamo settings

--- bootstrap-app.sh
+++ bootstrap-app.sh
@@ -14,7 +14,7 @@ until "$dynamoReady"; do
   fi;
 done
 
-AWS_ACCESS_KEY_ID=1 AWS_SECRET_ACCESS_KEY=2 aws dynamodb create-table --table-name carAdverts --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 --endpoint-url "$endpoint" --region us-east-1
+aws dynamodb create-table --table-name carAdverts --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 --endpoint-url "$endpoint" --region us-east-1
 
 unzip target/universal/car-adverts-assignment-1.0
 car-adverts-assignment-1.0/bin/car-adverts-assignment
